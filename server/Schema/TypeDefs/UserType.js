3ad7feef30f913b4ffbd29b3575d9da4bcc1d5f7
Abdullah0332 <abdullah.khan10032@gmail.com>
2021-10-01 00:41:05 +0500

    'push'

---
+++ server/Schema/TypeDefs/UserType.js
@@ -0,0 +1,24 @@
+const { GraphQLString, GraphQLObjectType, GraphQLInt } = require("graphql");
+
+const UserType = new GraphQLObjectType({
+  name: "User",
+  fields: () => ({
+    id: {
+      type: GraphQLInt,
+    },
+    firstName: {
+      type: GraphQLString,
+    },
+    lastName: {
+      type: GraphQLString,
+    },
+    email: {
+      type: GraphQLString,
+    },
+    password: {
+      type: GraphQLString,
+    },
+  }),
+});
+
+module.exports = UserType;
